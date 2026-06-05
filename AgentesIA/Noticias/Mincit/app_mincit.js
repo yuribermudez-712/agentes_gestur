92cd3331603f5f729d4f0caa52f39a2d9b2f2221
Yuri <yuri.bermudez@eudea.edu.co>
2026-06-05 00:38:50 +0000

    ajutes

--- AgentesIA/Noticias/Mincit/app_mincit.js
+++ AgentesIA/Noticias/Mincit/app_mincit.js
@@ -44,7 +44,7 @@ async function sendFileOD({ path, nombreArchivo }) {
 
 (async () => {
 
-    const browser = await chromium.launch({ headless: true });
+    const browser = await chromium.launch({ headless: true, executablePath: '/snap/bin/chromium' });
     const context = await browser.newContext({
         userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
     });
